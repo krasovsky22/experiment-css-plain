304f78f16d137ec39037e2ca08611a8b190017bb
vladk <vlad_krasovsky@yahoo.com>
2020-08-16 14:20:15 -0700

    Updates

---
+++ precache-manifest.d07dc0c1947f35e085e981d522ce7b50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b852230a2dcf0cf71e1a8b7d52696135",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21bf6d4794a1a0ef35f4",
+    "url": "./static/css/main.a96a7988.chunk.css"
+  },
+  {
+    "revision": "686217af66701cd347c7",
+    "url": "./static/js/2.b7381bf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b7381bf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21bf6d4794a1a0ef35f4",
+    "url": "./static/js/main.babe19fa.chunk.js"
+  },
+  {
+    "revision": "0fca533e34f84eefec1c",
+    "url": "./static/js/runtime-main.44bd5a6e.js"
+  },
+  {
+    "revision": "1b550407f76b86f03b893ca918f016de",
+    "url": "./static/media/cloud.1b550407.png"
+  },
+  {
+    "revision": "0a0438a1e47c4debd64e865f1d4c3f9c",
+    "url": "./static/media/earth.0a0438a1.png"
+  },
+  {
+    "revision": "de71a53450745a24df70d054fe795c7a",
+    "url": "./static/media/plane.de71a534.png"
+  }
+]);
