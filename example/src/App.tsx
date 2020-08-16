97844cec214f984b815716a6ef2ea03b067810dc
vladk <vlad_krasovsky@yahoo.com>
2020-08-16 12:57:48 -0700

    css plain

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-import { ExampleComponent } from 'plane'
+import PlaneExperiment from 'plane'
 import 'plane/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div className='full-page'>
+      <PlaneExperiment />
+    </div>
+  )
 }
 
 export default App
